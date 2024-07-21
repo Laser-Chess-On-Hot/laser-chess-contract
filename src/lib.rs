1b2ce441fb28716530d3959e00f036debf459ec0
advanced-user <stolbetskiy.yura@gmail.com>
2024-07-22 00:49:12 +0300

    impl place bet

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,35 @@
-// Find all our documentation at https://docs.near.org
-use near_sdk::{log, near};
+use near_sdk::{env, json_types::U128, near, store::LookupMap, AccountId};
 
-// Define the contract structure
 #[near(contract_state)]
 pub struct Contract {
-    greeting: String,
+    users_bets: LookupMap<AccountId, U128>,
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract {
-    fn default() -> Self {
+#[near]
+impl Contract {         
+    #[init]
+    #[private] 
+    pub fn init() -> Self {
         Self {
-            greeting: "Hello".to_string(),
+            users_bets: LookupMap::new(b"m"),
         }
     }
+
+    #[payable]
+    pub fn place_bet(&mut self) {
+        let sender_id = env::predecessor_account_id();
+        let attached_deposit = env::attached_deposit().as_yoctonear(); 
+
+        assert!(attached_deposit > 0, "Must attach some NEAR tokens to place a bet");
+
+        if self.users_bets.get(&sender_id).is_some() {
+            panic!("The user has already placed a bet")
+        }
+
+        let new_bet = U128(attached_deposit);
+        self.users_bets.insert(sender_id, new_bet);
+    }
 }
 
-// Implement the contract structure
-#[near]
-impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_GREETING
-    pub fn get_greeting(&self) -> String {
-        self.greeting.clone()
-    }
-
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, greeting: String) {
-        log!("Saving greeting: {greeting}");
-        self.greeting = greeting;
-    }
-}
-
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(contract.get_greeting(), "Hello");
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(contract.get_greeting(), "howdy");
-    }
-}
+mod tests { }
